954e750b633b501aa903d400dd058f087afed0c7
Maxwell117 <33138301+Maxwell117@users.noreply.github.com>
2023-03-20 21:10:08 -0400

    Add files via upload

---
+++ tacoparser/LoggingKata.Test/TacoParserTests.cs
@@ -0,0 +1,61 @@
+using System;
+using Xunit;
+
+namespace LoggingKata.Test
+{
+    public class TacoParserTests
+    {
+        [Fact]
+        public void ShouldDoSomething()
+        {
+            // TODO: Complete Something, if anything
+
+            //Arrange
+            var tacoParser = new TacoParser();
+
+            //Act
+            var actual = tacoParser.Parse("34.073638, -84.677017, Taco Bell Acwort...");
+
+            //Assert
+            Assert.NotNull(actual);
+
+        }
+
+        [Theory]
+        [InlineData("34.073638, -84.677017, Taco Bell Acwort...", -84.677017)]
+        public void ShouldParseLongitude(string line, double expected)
+        {
+            // TODO: Complete - "line" represents input data we will Parse to
+            //       extract the Longitude.  Your .csv file will have many of these lines,
+            //       each representing a TacoBell location
+
+            //Arrange
+            var tacoparserinstance = new TacoParser();
+
+            //Act
+            var actual = tacoparserinstance.Parse(line).location.longitude;
+            //Assert
+            Assert.equal(expected,actual);
+        }
+ 
+         [Theory]
+        [InlineData("34.073638, -84.677017, Taco Bell Acwort...", 34.677017)]
+        public void ShouldParseLat(string line, double expected)
+        {
+            // TODO: Complete - "line" represents input data we will Parse to
+            //       extract the Longitude.  Your .csv file will have many of these lines,
+            //       each representing a TacoBell location
+
+            //Arrange
+            var tacoparserinstance = new TacoParser();
+
+            //Act
+            var actual = tacoparserinstance.Parse(line).location.latitude;
+            //Assert
+            Assert.equal(expected,actual);
+        }
+
+        //TODO: Create a test ShouldParseLatitude
+
+    }
+}
